f5ed0fde3d0f004e20dda3312dbf4455070925ba
matheusWegner <matheus.christo@dbfactory.com.br>
2024-09-03 15:09:11 -0300

    first commit

---
+++ src/test/java/br/com/tads/manutencao_equipamento_api/ManutencaoEquipamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tads.manutencao_equipamento_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoEquipamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
